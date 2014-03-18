9a98d1016c8ac4ed03142815c6c745c3ec742180
Erwin Boskma <erwin@datarift.nl>
2014-03-19 00:58:07 +0100

    Removed some unnecessary files, disabled obsolete tests

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,22 +1,22 @@
-require 'spec_helper'
-
-describe WelcomeController do
-  let(:user) { create(:user) }
-  
-  it "renders the index view without a user" do
-    get :index
-    expect(response.status).to eq 200
-    expect(response).to render_template 'index'
-  end
-  
-  it "renders the dashboard with the Angular template" do
-    sign_in user
-    get :dashboard
-    expect(response).to render_template 'dashboard'
-  end
-  
-  it "redirects from the dashboard to index without a user" do
-    get :dashboard
-    expect(response).to be_redirect
-  end
-end
+# require 'spec_helper'
+# 
+# describe WelcomeController do
+#   let(:user) { create(:user) }
+#   
+#   it "renders the index view without a user" do
+#     get :index
+#     expect(response.status).to eq 200
+#     expect(response).to render_template 'index'
+#   end
+#   
+#   it "renders the dashboard with the Angular template" do
+#     sign_in user
+#     get :dashboard
+#     expect(response).to render_template 'dashboard'
+#   end
+#   
+#   it "redirects from the dashboard to index without a user" do
+#     get :dashboard
+#     expect(response).to be_redirect
+#   end
+# end
