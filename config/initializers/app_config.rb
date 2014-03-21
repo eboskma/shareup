9006a6d30ceb1b6049383f349069d3b107855c84
Erwin Boskma <erwin@datarift.nl>
2014-03-21 10:05:12 +0100

    Made the configuration more robust

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,5 +1,7 @@
 require 'ostruct'
 require 'yaml'
 
-config = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml')) || {}
+config_file = File.join(Rails.root, 'config', 'app_config.yml')
+
+config = File.exist?(config_file) && YAML.load_file(config_file) || {}
 AppConfig = OpenStruct.new(config[Rails.env] || {})
